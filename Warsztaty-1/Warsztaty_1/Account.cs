c783d519fd716e4ec01a1009cf0fc9def76d2266
Pablo <pawelstozynski@gmail.com>
2018-12-17 18:39:53 +0100

    Workshop 1

---
+++ Warsztaty-1/Warsztaty_1/Account.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warsztaty_1
+{
+    class Account
+    {
+        public string name;
+        private double amount;
+
+        public Account(string name)
+        {
+            this.name = name;
+            this.amount = 100.0;
+        }
+
+        public void Deposit(double amount)
+        {
+            this.amount += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (this.amount >= amount)
+            {
+                this.amount -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Not enough money :(");
+            }
+        }
+
+        public double GetAmount()
+        {
+            return this.amount;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.amount} PLN on {this.name}";
+        }
+
+    }
+}
---
+++ Warsztaty-1/Warsztaty_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warsztaty_1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Account normal = new Account();
+            normal.name = "Konto zwykłe";
+            normal.amount = 2000.0;
+
+            Account savings = new Account();
+            savings.name = "Konto oszczędnościowe";
+            savings.amount = 5000.0;*/
+
+            Account normal = new Account("Konto zwykłe");
+            Account savings = new Account("Konto oszczędnościowe");
+
+            normal.Deposit(5000.0);
+            normal.Withdraw(2000.0);
+
+            Console.WriteLine(normal.ToString());
+            Console.WriteLine(savings.ToString());
+            Console.WriteLine(normal.GetAmount());
+
+            Console.ReadKey();
+        }
+    }
+}
